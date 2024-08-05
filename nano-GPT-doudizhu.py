57bbad78162bfbed018008bcbf40237be3c9f667
sharmt1411 <1411654935@qq.com>
2024-08-05 14:24:19 +0800

    增加data目录

--- nano-GPT-doudizhu.py
+++ nano-GPT-doudizhu.py
@@ -3,4 +3,7 @@
 未在本地训练，如在本地训练，需要安装pytorch，cuda
 并且数据需要移出文件夹
 Colab:https://colab.research.google.com/drive/1h42HcnT0zJ3NidvOqs-IXWvXNvBum2I4?usp=sharing
+
+simulate为采集对局信息的模拟器，可以自己编写，也可以使用现成的模拟器生成数据。
+
 """
